5a64d2e2c01b18b7ef3b4bbdb943727f58419e67
Renata <renatagomescaetano@gmail.com>
2021-02-11 18:51:15 -0200

    Commit teste

---
+++ Teste/src/main/java/com/example/Teste/repository/ProdutoRepository.java
@@ -0,0 +1,7 @@
+package com.example.Teste.repository;
+
+import com.example.Teste.model.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+}
---
+++ Teste/src/main/java/com/example/Teste/service/ProdutoServiceImpl.java
@@ -0,0 +1,24 @@
+package com.example.Teste.service;
+
+import com.example.Teste.framework.CrudServiceImpl;
+import com.example.Teste.model.Produto;
+import com.example.Teste.repository.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProdutoServiceImpl extends CrudServiceImpl<Produto, Long> implements ProdutoService{
+
+    @Autowired
+    ProdutoRepository produtoRepository;
+
+    @Override
+    public JpaRepository<Produto, Long> getRepository() {
+        return produtoRepository;
+    }
+
+
+
+
+}
